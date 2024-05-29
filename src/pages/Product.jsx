403eb645da1b16c42dafa669657049e30e52867d
azikodev <aziko.coder@gmail.com>
2024-05-29 23:40:52 +0500

    my shop

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -17,28 +17,38 @@ function Product() {
   const [productAmount, setProductAmount] = useState(1);
 
   const setAmount = (type) => {
-    if (type == "decrease" && productAmount > 1) {
+    if (type === "decrease" && productAmount > 1) {
       setProductAmount((prev) => prev - 1);
-    } else if (type == "increase") {
+    } else if (type === "increase") {
       setProductAmount((prev) => prev + 1);
     }
   };
+
   const addToBag = () => {
-    const newProdact = {
+    const newProduct = {
       ...product,
       amount: productAmount,
     };
 
-    dispatch(addProduct(newProdact));
+    dispatch(addProduct(newProduct));
   };
+
+  if (!product.images || product.images.length < 3) {
+    return null;
+  }
+
   return (
     <div className="max-w-6xl mx-auto flex flex-col items-center">
       <h1 className="text-3xl font-bold">{product.title}</h1>
       <div className="carousel carousel-center max-w-4xl mx-auto p-4 space-x-4 bg-neutral rounded-box">
         {product.images.map((image) => {
           return (
-            <div key={image} className="carousel-itme">
-              <img src={image} className="rounded-box h-96" />
+            <div key={image} className="carousel-item">
+              <img
+                src={image}
+                className="rounded-box h-96"
+                alt="Product Image"
+              />
             </div>
           );
         })}
@@ -53,8 +63,8 @@ function Product() {
         <h3>{productAmount}</h3>
         <button
           onClick={() => setAmount("decrease")}
-          className="btn btn-secondary "
-          disabled={productAmount == 1 ? true : false}
+          className="btn btn-secondary"
+          disabled={productAmount === 1}
         >
           -
         </button>
